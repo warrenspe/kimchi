aec7fd31ae43282d7dffeb23841463795fc29aae
Warren Spencer <warrenspencer27@gmail.com>
2016-07-16 22:09:13 -0600

    pump: Moves some developer documentation into pump.c

--- pump/pump.c
+++ pump/pump.c
@@ -16,8 +16,42 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "headers/pump.h"
 
+/* DevNote: The memory format this library uses to serialize objects is as follows:
+ *
+ * [Type][Size][Serialization]
+ *
+ * Type: 1 byte.
+ *     Bitflag representing what type was serialized here. See pump.h for type definitions.
+ *
+ * Size: X bytes.
+ *     The number of bytes the size field consumes is determined by the first byte which has its first bit set to 1.
+ *     Example: [00001111][00000000][11111111] <- size is 3 bytes long; first byte with a 1 in the first position
+ *              indicates that it is the final byte.  Note that only the first 7 bits of each byte contribute towards
+ *              the value that is serialized in the size field (the first of each is just a continuation flag).
+ *              It was implemented in this manner to be more frugal regarding the # of stored bytes; as using only
+ *              7 bits per byte is more efficient than dedicating an entire byte to represent the length of the size
+ *              field for all serialized objects up to 72057594037927936 (2**56) bytes in size. (The maximum we're able
+ *              to serialize being 2**63).  Since we expect the vast majority of serialized objects to be of a size
+ *              smaller than 2**56 it was implemented in this fashion.
+ *
+ * Serialization: {Size} bytes.
+ *     The serialization of the object.  Certain objects (ex, iterables) have their own substructures in this section.
+ */
+
+/* DevNote: To extend this library with the capability to serialize further objects, the following steps must be taken.
+ *     Follow the existing conventions in place in each of the files when making changes.
+ *
+ *     1. Add a new entry to pump/headers/pump.h: Type Definitions.
+ *     2. Add an entry to if block chain in pump/utils/types.c discerning whether a generic python object is of your type.
+ *     3. Add cases to the switches in pump/utils/serialize.c to serialize and deserialize objects of your type.
+ *     4. Add a new file to pump/serializers/ to serialize & deserialize your type.
+ *        Ensure that the Python Object returned from your deserialization has a ref count of 1.
+ *     5. Add a new unit test for your type to the pump/tests/ directory.
+ *        It will be automatically included in the unit tests.
+ */
+
+#include "headers/pump.h"
 
 PyObject *deflate(PyObject *self, PyObject *args) {
 /* API function which serializes a python object.
