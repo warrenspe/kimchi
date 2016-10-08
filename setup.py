ce0572f057094888bee5efe64ddd0bcd36130bf0
Warren Spencer <warrenspencer27@gmail.com>
2016-10-07 23:21:28 -0600

    kimchi: Adds missing files to the dist

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 import distutils.core
 
 DEBUG_COMPILE_ARGS = None
-VERSION = "2.0"
+VERSION = "2.0.1"
 NAME = "kimchi"
 
 if "DEBUG" in os.environ:
@@ -26,7 +26,7 @@ distutils.core.setup(
         distutils.core.Extension(
             NAME,
             sources = ['{0}/{0}.c'.format(NAME)],
-            include_dirs = [NAME, '%s/utils' % NAME, '%s/serializers' % NAME],
+            include_dirs = [NAME],
             extra_compile_args=DEBUG_COMPILE_ARGS
         )
      ]
